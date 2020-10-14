d4556dd29e7cc1b0fee6a044503dae02be760c80
Gustavo Gómez <luis.gomez6835@alumnos.udg.mx>
2020-10-14 14:51:51 -0500

    Distancia Euclidiana

--- algoritmos.py
+++ algoritmos.py
@@ -1,16 +1,9 @@
+import math 
 def distancia_euclidiana(x_1, y_1, x_2, y_2):
-    """ Calcula la distancia euclidiana
-
-    Devuelve el resultado de la formula
-
-    También se le conoe a la fórmula como:
-    distancia entre dos puntos
-
-    Parámetros:
-    x_1 -- origen_x
-    y_1 -- origen_y
-    x_2 -- destino_x
-    y_2 -- destino_y
-
-    """
+    x = x_2 - x_1
+    x1=pow(x,2)
+    y = y_2 - y_1
+    y1=pow(y,2)
+    resultado = x1+y1
+    return(math.sqrt(resultado))
     
